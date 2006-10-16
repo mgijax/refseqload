98830650893bba8793550b4881863ab221c0e473
Sharon Giannatto <Sharon.Giannatto@jax.org>
2006-10-16 14:57:10 +0000

    tr7894

--- bin/refseqload.sh
+++ bin/refseqload.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 #
-#  $Header
-#  $Name
-#
 #  refseqload.sh
 ###########################################################################
 #
@@ -18,8 +15,9 @@
 #
 #  Inputs:
 #
-#      - Common configuration file (/usr/local/mgi/etc/common.config.sh)
-#      - RefSeq load configuration file (refseqload.config)
+#      - Common configuration file -
+#		/usr/local/mgiconfig/master.config.sh
+#      - RefSeq load configuration file - refseqload.config
 #      - One or more RefSeq input files 
 #
 #  Outputs:
@@ -68,18 +66,11 @@ fi
 #
 #  Establish the configuration file names.
 #
-CONFIG_COMMON=`pwd`/common.config.sh
 CONFIG_LOAD=`pwd`/refseqload.config
-echo ${CONFIG_LOAD}
 
 #
-#  Make sure the configuration files are readable.
+#  Make sure the configuration file is readable.
 #
-if [ ! -r ${CONFIG_COMMON} ]
-then
-    echo "Cannot read configuration file: ${CONFIG_COMMON}" | tee -a ${LOG}
-    exit 1
-fi
 if [ ! -r ${CONFIG_LOAD} ]
 then
     echo "Cannot read configuration file: ${CONFIG_LOAD}" | tee -a ${LOG}
@@ -87,14 +78,19 @@ then
 fi
 
 #
-# Source the common configuration files
-#
-. ${CONFIG_COMMON}
-
-#
 # Source the GenBank Load configuration files
 #
 . ${CONFIG_LOAD}
+
+#
+#  Make sure the master configuration file is readable
+#
+
+if [ ! -r ${CONFIG_MASTER} ]
+then
+    echo "Cannot read configuration file: ${CONFIG_MASTER}"
+    exit 1
+fi
 
 echo "javaruntime:${JAVARUNTIMEOPTS}"
 echo "classpath:${CLASSPATH}"
@@ -118,24 +114,6 @@ else
 fi
 
 #
-#  Function that performs cleanup tasks for the job stream prior to
-#  termination.
-#
-shutDown ()
-{
-    #
-    # report location of logs
-    #
-    echo "\nSee logs at ${LOGDIR}\n" >> ${LOG_PROC}
-
-    #
-    # call DLA library function
-    #
-    postload
-
-}
-
-#
 # Function that runs to java load
 #
 
@@ -145,28 +123,26 @@ run ()
     # log time and input files to process
     #
     echo "\n`date`" >> ${LOG_PROC}
-    echo "Files read from stdin: ${APP_CAT_METHOD} ${APP_INFILES}" | tee -a ${LOG_DIAG} ${LOG_PROC}
+    echo "Files read from stdin: ${APP_CAT_METHOD} ${APP_INFILES}" | \
+	tee -a ${LOG_DIAG} ${LOG_PROC}
     #
     # run refseqload
     #
     ${APP_CAT_METHOD}  ${APP_INFILES}  | \
 	${JAVA} ${JAVARUNTIMEOPTS} -classpath ${CLASSPATH} \
-	-DCONFIG=${CONFIG_COMMON},${CONFIG_LOAD} \
+	-DCONFIG=${CONFIG_MASTER},${CONFIG_LOAD} \
 	-DJOBKEY=${JOBKEY} ${DLA_START}
 
     STAT=$?
-    if [ ${STAT} -ne 0 ]
-    then
-	echo "refseqload processing failed.  \
-	    Return status: ${STAT}" >> ${LOG_PROC}
-	shutDown
-	exit 1
-    fi
-    echo "refseqload completed successfully" >> ${LOG_PROC}
+    checkStatus ${STAT} "${REFSEQLOAD}"
 }
 
 ##################################################################
+##################################################################
+#
 # main
+#
+##################################################################
 ##################################################################
 
 #
@@ -182,7 +158,9 @@ cleanDir ${OUTPUTDIR} ${RPTDIR}
 
 # if we are processing the non-cums (incremental mode)
 # get a set of files, 1 file or set < configured value in MB (compressed)
+
 echo "checking APP_RADAR_INPUT: ${APP_RADAR_INPUT}"
+
 if [ ${APP_RADAR_INPUT} = true -a ${SEQ_LOAD_MODE} = incremental ]
 then
     echo 'Getting files to Process' | tee -a ${LOG_DIAG}
@@ -192,13 +170,8 @@ then
     APP_INFILES=`${RADAR_DBUTILS}/bin/getFilesToProcess.csh \
         ${RADAR_DBSCHEMADIR} ${JOBSTREAM} ${SEQ_PROVIDER} ${APP_RADAR_MAX}` 
     STAT=$?
-    if [ ${STAT} -ne 0 ]
-    then
-        echo "getFilesToProcess.csh failed.  \
-            Return status: ${STAT}" >> ${LOG_PROC}
-        shutDown
-        exit 1
-    fi
+    checkStatus ${STAT} "${RADAR_DBUTILS}/bin/getFilesToProcess.csh"
+
     # if no input files report and shutdown gracefully
     if [ "${APP_INFILES}" = "" ]
     then
@@ -206,21 +179,21 @@ then
         shutDown
         exit 0
     fi
-    # save to new var, if we are processing repeats APP_INFILES
-    # is reassigned and we won't be able to log the processed files properly
-    FILES_PROCESSED=${APP_INFILES}
-    echo 'Done getting files to Process'
+
+    echo 'Done getting files to Process' | tee -a ${LOG_DIAG}
 
 fi
+
+# save to new variable, when repeats are processed APP_INFILES
+# use FILES_PROCESSED to log processed files
+FILES_PROCESSED=${APP_INFILES}
+
 # if we get here then APP_INFILES not set in configuration this is an error
-#echo "APP_INFILES=${APP_INFILES}"
 if [ "${APP_INFILES}" = "" ]
 then
     # set STAT for endJobStream.py called from postload in shutDown
     STAT=1
-    echo "APP_RADAR_INPUT=${APP_RADAR_INPUT}. SEQ_LOAD_MODE=${SEQ_LOAD_MODE}. Check that APP_INFILES has been configured. Return status: ${STAT}" | tee -a ${LOG_PROC}
-    shutDown
-    exit 1
+    checkStatus ${STAT} "APP_RADAR_INPUT=${APP_RADAR_INPUT}. SEQ_LOAD_MODE=${SEQ_LOAD_MODE}. Check that  APP_INFILES has been configured."
 fi
 
 #
@@ -249,9 +222,6 @@ then
 	# run the load
 	run
 
-	# remove the repeat file we just ran
-	#echo "Removing ${APP_REPEAT_TO_PROCESS}"
-	#rm ${APP_REPEAT_TO_PROCESS}
         echo "saving repeat file ${APP_REPEAT_TO_PROCESS}.${ctr}"
         mv ${APP_REPEAT_TO_PROCESS} ${APP_REPEAT_TO_PROCESS}.${ctr}
         ctr=`expr ${ctr} + 1`
@@ -260,24 +230,17 @@ fi
 
 # if we are processing the non-cums (incremental mode)
 # log the non-cums we processed
-if [ ${APP_RADAR_INPUT} = true -a ${SEQ_LOAD_MODE} = incremental ]
+if [ ${SEQ_LOAD_MODE} = incremental ]
 then
-    echo 'Logging processed files'
+    echo "Logging processed files ${FILES_PROCESSED}" >> ${LOG_DIAG}
     for file in ${FILES_PROCESSED}
     do
-        ${RADARDBUTILSDIR}/bin/logProcessedFile.csh ${RADAR_DBSCHEMADIR} \
+        ${RADAR_DBUTILS}/bin/logProcessedFile.csh ${RADAR_DBSCHEMADIR} \
             ${JOBKEY} ${file} ${SEQ_PROVIDER}
         STAT=$?
-        if [ ${STAT} -ne 0 ]
-        then
-            echo "logProcessedFile.csh failed.  \
-                Return status: ${STAT}" >> ${LOG_PROC}
-            shutDown
-            exit 1
-        fi
-
+	checkStatus ${STAT} "${RADAR_DBUTILS}/bin/logProcessedFile.csh"
     done
-    echo 'Done logging processed files'
+    echo 'Done logging processed files' >> ${LOG_DIAG}
 fi
 
 #
@@ -289,12 +252,7 @@ echo "\n`date`" >> ${LOG_DIAG}
 
 ${APP_MSP_QCRPT} ${RADAR_DBSCHEMADIR} ${MGD_DBNAME} ${JOBKEY} ${RPTDIR}
 STAT=$?
-if [ ${STAT} -ne 0 ]
-then
-    echo "Running MSP QC reports failed.  Return status: ${STAT}" >> ${LOG_PROC}
-    shutDown
-    exit 1
-fi
+checkStatus ${STAT} ${APP_MSP_QCRPT}
 
 #
 # run seqload qc reports
@@ -305,12 +263,7 @@ echo "\n`date`" >> ${LOG_DIAG}
 
 ${APP_SEQ_QCRPT} ${RADAR_DBSCHEMADIR} ${MGD_DBNAME} ${JOBKEY} ${RPTDIR}
 STAT=$?
-if [ ${STAT} -ne 0 ]
-then
-    echo "Running seqloader QC reports failed.  Return status: ${STAT}" >> ${LOG_PROC}
-    shutDown
-    exit 1
-fi
+checkStatus ${STAT} ${APP_SEQ_QCRPT}
 
 #
 # run postload cleanup and email logs
@@ -318,28 +271,3 @@ fi
 shutDown
 
 exit 0
-
-$Log
-
-###########################################################################
-#
-# Warranty Disclaimer and Copyright Notice
-#
-#  THE JACKSON LABORATORY MAKES NO REPRESENTATION ABOUT THE SUITABILITY OR
-#  ACCURACY OF THIS SOFTWARE OR DATA FOR ANY PURPOSE, AND MAKES NO WARRANTIES,
-#  EITHER EXPRESS OR IMPLIED, INCLUDING MERCHANTABILITY AND FITNESS FOR A
-#  PARTICULAR PURPOSE OR THAT THE USE OF THIS SOFTWARE OR DATA WILL NOT
-#  INFRINGE ANY THIRD PARTY PATENTS, COPYRIGHTS, TRADEMARKS, OR OTHER RIGHTS.
-#  THE SOFTWARE AND DATA ARE PROVIDED "AS IS".
-#
-#  This software and data are provided to enhance knowledge and encourage
-#  progress in the scientific community and are to be used only for research
-#  and educational purposes.  Any reproduction or use for commercial purpose
-#  is prohibited without the prior express written permission of The Jackson
-#  Laboratory.
-#
-# Copyright \251 1996, 1999, 2002, 2003 by The Jackson Laboratory
-#
-# All Rights Reserved
-#
-###########################################################################
