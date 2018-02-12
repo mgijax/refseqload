a21730aa6d725e094dbc04b4cd901dfdbafa829d
Sharon Cousins Giannatto <sharon.giannatto@jax.org>
2018-02-12 14:49:16 -0500

    updated setval to max(key) FROM max(key) + 1

--- bin/refseqload.sh
+++ bin/refseqload.sh
@@ -232,8 +232,8 @@ fi
 # update serialization on mgi_reference_assoc, seq_source_assoc
 cat - <<EOSQL | ${PG_DBUTILS}/bin/doisql.csh $0 | tee -a ${LOG_DIAG}
 
-select setval('mgi_reference_assoc_seq', (select max(_Assoc_key) + 1 from MGI_Reference_Assoc));
-select setval('seq_source_assoc_seq', (select max(_Assoc_key) + 1 from SEQ_Source_Assoc));
+select setval('mgi_reference_assoc_seq', (select max(_Assoc_key) from MGI_Reference_Assoc));
+select setval('seq_source_assoc_seq', (select max(_Assoc_key) from SEQ_Source_Assoc));
 
 EOSQL
 
